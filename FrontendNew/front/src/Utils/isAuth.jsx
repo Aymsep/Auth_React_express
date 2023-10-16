bc5fa365e4d71e57590e4193596a758eaccc0c3b
Aymsep <71574687+Aymsep@users.noreply.github.com>
2023-10-16 21:36:45 +0000

    done

--- FrontendNew/front/src/Utils/isAuth.jsx
+++ FrontendNew/front/src/Utils/isAuth.jsx
@@ -1,28 +1,8 @@
-export default function isAuth() {
-    // Get the JWT token from localStorage
-    const jwt = localStorage.token;
-  
-    if (!jwt) {
-      return false; // Token is not present
+export default function isAuth(){
+    const jwt = localStorage.token
+    if(!jwt){
+        return false
     }
-  
-    // Send a request to the server to validate the token
-    return fetch('/api/validateToken', {
-      method: 'POST', // Adjust the HTTP method as needed
-      headers: {
-        'Authorization': jwt,
-        'Content-Type': 'application/json',
-      },
-    })
-      .then((response) => {
-        if (response.status === 200) {
-          return jwt; // Token is valid
-        } else {
-          return false; // Token is not valid
-        }
-      })
-      .catch((error) => {
-        return false; // Error occurred, treat as if token is not valid
-      });
-  }
-  
+    return jwt
+}
+
